041d49a15a3ac3b8ac64c00afcff39c68d985100
Tran Thanh Dat <neban0444@gmail.com>
2023-12-02 19:35:07 +0700

    first commit

---
+++ OutfitAssistant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OutfitAssistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
